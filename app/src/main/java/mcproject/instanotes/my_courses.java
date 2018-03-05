c721e27c4dc8490b08efbeb464d5244e75842c89
Avdesh <avdesh15135@iiitd.ac.in>
2018-03-05 17:21:47 +0530

    picassa

--- app/src/main/java/mcproject/instanotes/my_courses.java
+++ app/src/main/java/mcproject/instanotes/my_courses.java
@@ -1,8 +1,11 @@
 package mcproject.instanotes;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,12 +15,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.squareup.picasso.Picasso;
 
 public class my_courses extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
+    public GoogleApiClient mGoogleApiClient;
+    public TextView navUsername,credit,email;
+    public ImageView dp;
+    private final Context mContext = this;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -41,8 +60,40 @@ public class my_courses extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        dp = (ImageView) headerView.findViewById(R.id.dp);
+        navUsername = (TextView) headerView.findViewById(R.id.username);
+
+        credit = (TextView) headerView.findViewById(R.id.credit);
+        credit.setText("7 Credits");
+        email = (TextView) headerView.findViewById(R.id.credit);
+
         navigationView.setNavigationItemSelectedListener(this);
+//        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+//        if (opr.isDone()) {
+//            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+//            // and the GoogleSignInResult will be available instantly.
+//            //Log.d(TAG, "Got cached sign-in");
+//            GoogleSignInResult result = opr.get();
+//            if (result.isSuccess()) {
+                // Signed in successfully, show authenticated UI.
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null) {
+//                GoogleSignInAccount acct = result.getSignInAccount();
+                //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+                // Views inside NavigationView's header
+                navUsername.setText(acct.getDisplayName());
+                email.setText(acct.getEmail());
+                Uri uri = acct.getPhotoUrl();
+                Picasso.with(mContext)
+                        .load(uri)
+                        .placeholder(android.R.drawable.sym_def_app_icon)
+                        .error(android.R.drawable.sym_def_app_icon)
+                        .into(dp);
+            }
     }
+
 
     @Override
     public void onBackPressed() {
