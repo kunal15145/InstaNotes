87222bd09835f94bb576278b0f7749beb4ed282a
kunal15145 <kunal15145@iiitd.ac.in>
2018-03-08 19:17:14 +0530

    removed google sign in

--- app/src/main/java/mcproject/instanotes/IntroductionScreen.java
+++ app/src/main/java/mcproject/instanotes/IntroductionScreen.java
@@ -3,6 +3,8 @@ package mcproject.instanotes;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -17,13 +19,25 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.provider.Provider;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntroductionScreen extends AppCompatActivity {
 
@@ -31,28 +45,10 @@ public class IntroductionScreen extends AppCompatActivity {
     private PagerAdapter ViewPagerAdapter;
     private LinearLayout AllDots;
     private TextView[] dots;
+    private SignInButton signInButton;
     private int[] FeatureScreenLayouts;
     private CheckFirstTimeLaunch checkFirstTimeLaunch;
-    private SignInButton signinbutton;
-    private GoogleSignInClient mGoogleSignInClient;
-    private final int SIGN_IN = 9001;
-    static final String kl = "dasdsa";
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            GoogleSignInAccount account;
-            try {
-                account = task.getResult(ApiException.class);
-                updateUI(account);
-            } catch (ApiException e) {
-                Log.w("TAG","Sign-In Failed");
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,19 +60,20 @@ public class IntroductionScreen extends AppCompatActivity {
         }
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_introduction_screen);
-        viewPager = findViewById(R.id.viewPageIntro);
-        AllDots = findViewById(R.id.Dots);
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(
-                GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this,signInOptions);
-        signinbutton = findViewById(R.id.signin);
-        signinbutton.setOnClickListener(new View.OnClickListener() {
+        // Login
+
+        signInButton = findViewById(R.id.signin);
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signin = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signin,SIGN_IN);
+                Intent intent = new Intent(IntroductionScreen.this,my_courses.class);
+                startActivity(intent);
             }
         });
+
+        // Introduction Screen
+        viewPager = findViewById(R.id.viewPageIntro);
+        AllDots = findViewById(R.id.Dots);
         FeatureScreenLayouts = new int[]{
                 R.layout.feature_screen1,
                 R.layout.feature_screen2,
@@ -116,15 +113,6 @@ public class IntroductionScreen extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null) {
-            updateUI(account);
-        }
-    }
-
     private void makeDotVisible(int i) {
         dots = new TextView[FeatureScreenLayouts.length];
         AllDots.removeAllViews();
@@ -141,19 +129,9 @@ public class IntroductionScreen extends AppCompatActivity {
     }
 
     private void launchMyCourses() {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null) {
-            updateUI(account);
-        }
         checkFirstTimeLaunch.setFirstLaunch(false);
-        startActivity(new Intent(IntroductionScreen.this,my_courses.class));
+        Intent intent = new Intent(IntroductionScreen.this,my_courses.class);
+        startActivity(intent);
         finish();
     }
-
-    private void updateUI(GoogleSignInAccount account) {
-        Intent sendIntent = new Intent(this,my_courses.class);
-        sendIntent.putExtra(kl,account.getEmail());
-        startActivity(sendIntent);
-    }
-
 }
--- app/src/main/java/mcproject/instanotes/my_courses.java
+++ app/src/main/java/mcproject/instanotes/my_courses.java
@@ -1,13 +1,9 @@
 package mcproject.instanotes;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -19,21 +15,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -41,10 +26,8 @@ public class my_courses extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    public GoogleApiClient mGoogleApiClient;
     public TextView navUsername,credit,email;
     public CircleImageView dp1;
-    private final Context mContext = this;
 
     @Override
 
@@ -52,7 +35,7 @@ public class my_courses extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
-        Button notifbutton=(Button)findViewById(R.id.button2);
+        Button notifbutton= findViewById(R.id.button2);
         notifbutton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -62,7 +45,7 @@ public class my_courses extends AppCompatActivity
                 startActivity(intent);
             }
         });
-        Button transButton=(Button)findViewById(R.id.button3);
+        Button transButton= findViewById(R.id.button3);
         transButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -72,7 +55,7 @@ public class my_courses extends AppCompatActivity
                 startActivity(intent2);
             }
         });
-        Button accbtn=(Button)findViewById(R.id.button4);
+        Button accbtn= findViewById(R.id.button4);
         accbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -82,7 +65,7 @@ public class my_courses extends AppCompatActivity
                 startActivity(intent3);
             }
         });
-        Button helpbtn=(Button)findViewById(R.id.button5);
+        Button helpbtn= findViewById(R.id.button5);
         helpbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -103,43 +86,38 @@ public class my_courses extends AppCompatActivity
                                        }
         );*/
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 Intent intent = new Intent(getApplicationContext(), join_courses.class);
                 startActivity(intent);
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
 
-        dp1 = (CircleImageView) headerView.findViewById(R.id.dp);
-        navUsername = (TextView) headerView.findViewById(R.id.username);
+        dp1 = headerView.findViewById(R.id.dp);
+        navUsername = headerView.findViewById(R.id.username);
 
-        credit = (TextView) headerView.findViewById(R.id.credit);
+        credit = headerView.findViewById(R.id.credit);
         credit.setText("7 Credits");
-        email = (TextView) headerView.findViewById(R.id.email);
+        email = headerView.findViewById(R.id.email);
 
         navigationView.setNavigationItemSelectedListener(this);
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if(acct!=null) {
-
-                navUsername.setText(acct.getDisplayName());
-                email.setText(acct.getEmail());
-                Uri uri = acct.getPhotoUrl();
+                navUsername.setText("dasdas");
+                email.setText("dasdas");
+                Uri uri = Uri.parse("dsadasd");
                 String TAG="url";
                 Log.d(TAG, String.valueOf(uri));
 
@@ -148,11 +126,6 @@ public class my_courses extends AppCompatActivity
                         .load(uri)
                         .error(android.R.drawable.sym_def_app_icon)
                         .into(dp1);
-//                Glide.with(this).load(uri)
-//                    .crossFade()
-//                    .thumbnail(0.5f)
-//                    .into(dp1);
-            }
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -169,24 +142,15 @@ public class my_courses extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my_courses, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -207,7 +171,7 @@ public class my_courses extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
