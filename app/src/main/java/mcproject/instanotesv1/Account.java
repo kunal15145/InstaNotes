893ace04b4d98549ec77e6b76110615ebdc8fc40
kunal15145 <kunal15145@iiitd.ac.in>
2018-03-11 14:33:05 +0530

    Added DataSnapshot in Account Activity

--- app/src/main/java/mcproject/instanotesv1/Account.java
+++ app/src/main/java/mcproject/instanotesv1/Account.java
@@ -12,6 +12,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -20,47 +25,54 @@ public class Account extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+    private FirebaseFirestore db;
+    private static final String INSTA_COINS = "InstaCoins";
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_account);
 
 
-        TextView sname=(TextView)findViewById(R.id.textView21);
-        TextView email=(TextView)findViewById(R.id.textView23);
-        TextView fname=(TextView)findViewById(R.id.textView13);
-        TextView credit=(TextView)findViewById(R.id.textView22);
+        TextView sname= findViewById(R.id.textView21);
+        TextView email= findViewById(R.id.textView23);
+        TextView fname= findViewById(R.id.textView13);
+        final TextView credit= findViewById(R.id.textView22);
         CircleImageView dp=findViewById(R.id.imageView13);
-//        dp = findViewById(R.id.dp);
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
-//        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-//        if(acct!=null) {
-//
-//
-////            Uri uri = acct.getPhotoUrl();
+        db = FirebaseFirestore.getInstance();
 
-////            dp.setImageURI(null);
-////            dp.setImageURI(uri);
-//
-//            email.setText(acct.getEmail());
-////            dp.setImageDrawable(getResources().getDrawable(R.drawable.person_photo));
-//            credit.setText("7");
-//
-//        }
         Uri uri = firebaseUser.getPhotoUrl();
         String str = firebaseUser.getDisplayName();
         String[] splitStr = str.split("\\s+");
         fname.setText(splitStr[0]);
         sname.setText(splitStr[1]);
         email.setText(firebaseUser.getEmail());
-        credit.setText("7 Credits");
+        db.collection("users")
+                .document(firebaseUser.getEmail())
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                        if(e!=null){
+                            Log.d("Failure Listining","ok");
+                            return;
+                        }
+                        else if(documentSnapshot!=null && documentSnapshot.exists()){
+                            Log.d("dasd", (String) documentSnapshot.get(INSTA_COINS));
+                            credit.setText((CharSequence) documentSnapshot.get(INSTA_COINS)+" credits");
+                        }
+                        else {
+                            Log.d("Current data",null);
+                        }
+                    }
+                });
         String TAG="url";
         Log.d(TAG, String.valueOf(uri));
 
-//
         Picasso.with(getApplicationContext())
                 .load(uri)
                 .error(android.R.drawable.sym_def_app_icon)
