7f99d2c4bd7588e5dcfb0f63a5ef3df85a13373b
Harshit Verma <harshitv@rocketmail.com>
2018-03-12 00:11:51 +0530

    Feedback with back button, status bar removed, search in join

--- app/src/main/java/mcproject/instanotesv1/Book_Activity.java
+++ app/src/main/java/mcproject/instanotesv1/Book_Activity.java
@@ -6,12 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 
 public class Book_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_book_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/mcproject/instanotesv1/feedback.java
+++ app/src/main/java/mcproject/instanotesv1/feedback.java
@@ -2,14 +2,46 @@ package mcproject.instanotesv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class feedback extends AppCompatActivity {
+
+    public RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_feedback);
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setTitle(R.string.Feedback_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // Initialize RatingBar
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    /**
+     * Display rating by calling getRating() method.
+     *
+     * @param view
+     */
+    public void rateMe(View view) {
+
+        Toast.makeText(getApplicationContext(),
+                String.valueOf(ratingBar.getRating()), Toast.LENGTH_SHORT).show();
+
     }
 }
