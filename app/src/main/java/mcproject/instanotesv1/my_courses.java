22317d59e41f3cd118322e052a1ab9bbd003e33d
Harshit Verma <harshitv@rocketmail.com>
2018-03-12 19:57:29 +0530

    All status bars removed, back buttons added on all screens

--- app/src/main/java/mcproject/instanotesv1/my_courses.java
+++ app/src/main/java/mcproject/instanotesv1/my_courses.java
@@ -58,7 +58,7 @@ public class my_courses extends AppCompatActivity
 //        getSupportActionBar().setLogo(R.drawable.notificon);
 //        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_my_courses);
 
@@ -90,8 +90,11 @@ public class my_courses extends AppCompatActivity
         firestore = FirebaseFirestore.getInstance();
 
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setTitle(R.string.mycourses_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -105,7 +108,7 @@ public class my_courses extends AppCompatActivity
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mActionBarToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
