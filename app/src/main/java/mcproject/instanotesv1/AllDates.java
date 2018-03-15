ce7a1eb39775ad89d731e16803afffdd0eeb715f
Harshit Verma <harshitv@rocketmail.com>
2018-03-15 23:48:49 +0530

    3 dots appear on each card in my courses

---
+++ app/src/main/java/mcproject/instanotesv1/AllDates.java
@@ -0,0 +1,13 @@
+package mcproject.instanotesv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllDates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_dates);
+    }
+}
