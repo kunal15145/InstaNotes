a87b6c5c65c0369cec795e8884a68ad4a75db097
Harshit Verma <harshitv@rocketmail.com>
2018-04-30 03:19:54 +0530

    nothing

--- app/src/main/java/mcproject/instanotesv1/HelpFrag1.java
+++ app/src/main/java/mcproject/instanotesv1/HelpFrag1.java
@@ -6,10 +6,31 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class HelpFrag1 extends Fragment{
 
+    private ImageView imageView;
+    boolean isImageFitToScreen;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.help_frag1,container,false);
+        imageView = view.findViewById(R.id.imageview);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
         return inflater.inflate(R.layout.help_frag1, container, false);
 
     }
