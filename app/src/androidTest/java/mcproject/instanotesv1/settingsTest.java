9d91dc31536e6d6234260a5cb9a4ef7ad96108d3
aarushi15002 <aarushiarya98@gmail.com>
2018-04-30 22:22:19 +0530

    Testing

---
+++ app/src/androidTest/java/mcproject/instanotesv1/settingsTest.java
@@ -0,0 +1,14 @@
+package mcproject.instanotesv1;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+import static org.junit.Assert.*;
+
+public class settingsTest {
+    @Rule
+    public ActivityTestRule<settings> activityActivityTestRule = new ActivityTestRule<>(settings.class, true, true);
+
+
+}
